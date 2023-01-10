494e34bc6243a9d071ebc3dbe605137a3ba4923d
Archknight23 <Aerodonicus@protonmail.com>
2023-01-10 02:31:24 -0600

    vtuber
     Author:    Archknight23 <Aerodonicus@protonmail.com>
     Date:      Tue Jan 10 02:31:24 2023 -0600
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Calender } from '@syncfusion/ej2-rcalendars';
+import { DatePicker } from '@syncfusion/ej2-react-calendars';
+import { HtmlEditor, Image, Inject, Link, QuickToolbar, RichTextEditorComponent, Toolbar } from '@syncfusion/ej2-react-richtexteditor';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: 0
+    };
+  }
+  
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState(prevState => ({
+        time: prevState.time + 1
+      }));
+    }, 1000);
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+  
+  render() {
+    return (
+      <div>
+        <CalenderComponent></CalenderComponent>
+        <DatePickerComponent></DatePickerComponent>
+        <RichTextEditor></RichTextEditor>
+      </div>
+    );
+  }
 }
 
 export default App;
